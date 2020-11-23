36ba201008327e371855d559be9b9f1df2d55d22
umedith <umedith03@gmail.com>
2020-11-23 08:04:55 +0200

    updated readme

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ class Config:
     """
     General configuration parent class
     """   
-    MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
+    NEWS_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
 class ProdConfig(Config):
     """
     Production configuration child class
