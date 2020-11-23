479d91d34b5f0471d68d33eabe929a9d685b1aab
umedith <umedith03@gmail.com>
2020-11-23 08:16:44 +0200

    removed the unnecessary codes

--- app/request.py
+++ app/request.py
@@ -118,13 +118,13 @@ def get_news(id):
             news_object = News(id,name,descriprion,url,category,)
     return news_object
 
-def search_news(_name):
-    search_news_url = 'https://api.themoviedb.org/3/search/movie?api_key={}&query={}'.format(api_key,movie_name)
-    with urllib.request.urlopen(search_movie_url) as url:
-        search_movie_data = url.read()
-        search_movie_response = json.loads(search_movie_data)
-        search_movie_results = None
-        if search_movie_response['results']:
-            search_movie_list = search_movie_response['results']
-            search_movie_results = process_results(search_movie_list)
-    return search_movie_results
+# def search_news(_name):
+#     search_news_url = 'https://api.themoviedb.org/3/search/movie?api_key={}&query={}'.format(api_key,movie_name)
+#     with urllib.request.urlopen(search_movie_url) as url:
+#         search_movie_data = url.read()
+#         search_movie_response = json.loads(search_movie_data)
+#         search_movie_results = None
+#         if search_movie_response['results']:
+#             search_movie_list = search_movie_response['results']
+#             search_movie_results = process_results(search_movie_list)
+#     return search_movie_results
